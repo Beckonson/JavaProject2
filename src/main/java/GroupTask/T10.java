156dbbe83bf223c525e4e715ddb350bff6b0a712
Becckonson <multititech11@gamil.com>
2024-01-07 11:13:33 +0100

    commit

--- src/main/java/GroupTask/T10.java
+++ src/main/java/GroupTask/T10.java
@@ -1,7 +1,32 @@
 package GroupTask;
+/*
+How can you remove all duplicates from ArrayList?
+ */
+import java.util.ArrayList;
 
 public class T10 {
     public static void main(String[] args) {
+        ArrayList <String> items = new ArrayList<>();
+        items.add("Book");
+        items.add("Pencil");
+        items.add("Laptop");
+        items.add("TV");
+        items.add("TV");
+        items.add("Book");
 
+        System.out.println("Before removal duplicates:");
+        items.forEach(x-> System.out.println(x));
+
+        for (int i = 0; i < items.size(); i++) {
+            for(int j = i+1; j < items.size(); j++ ){
+                if(items.get(i).equals(items.get(j))){
+                    items.remove(j);
+                    j--;
+                }
+            }
+        }
+        System.out.println("After removal duplicates: ");
+        items.forEach(c-> System.out.println(c));
     }
 }
+
